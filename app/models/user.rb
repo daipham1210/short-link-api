b83f06a8e5ab13bbdde5e6d9e5b0d259af417979
daipham <daipham1210@gmail.com>
2022-12-21 21:05:07 +0700

    feat: Authenticate user with devise jwt

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  include Devise::JWT::RevocationStrategies::JTIMatcher
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable,
+         :jwt_authenticatable, jwt_revocation_strategy: self
+
+  def jwt_payload
+    super
+  end
+end
